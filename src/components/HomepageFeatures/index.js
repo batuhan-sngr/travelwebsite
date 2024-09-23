f6eadbb1fe735eaf3ddaf5edf53f7ac5dc97d4c7
Emre Batuhan Sungur <batuhansungur@gmail.com>
2024-09-23 22:15:35 +0300

    edit1

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,28 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Share Your Experinces',
+    Svg: require('@site/static/img/share.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Sharing your experiences with others can inspire and provide valuable insights, 
+        helping them navigate similar challenges or opportunities.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Connect With Fellow Travelers',
+    Svg: require('@site/static/img/travelling.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Connecting with fellow travelers allows you to share experiences, tips, and recommendations, 
+        enriching your journey with diverse perspectives. 
+        It also creates opportunities for friendships and memories that can make your travels more fulfilling and enjoyable.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Now Create Your Post',
+    Svg: require('@site/static/img/images.svg').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
