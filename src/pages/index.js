aed22da9a1e32ae67ce30d9beb04aa3dad4fd073
Emre Batuhan Sungur <batuhansungur@gmail.com>
2024-09-24 03:19:51 +0300

    edit2

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,17 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Create a Post 📝
-          </Link>
+      <div className="hero_img">
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Create a Post 📝
+            </Link>
+          </div>
         </div>
       </div>
     </header>
